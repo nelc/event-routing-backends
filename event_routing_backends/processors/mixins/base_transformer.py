10f09c2836005868c7e70ec427220768ec85583a
Johan Castiblanco <johan.castiblanco@edunext.co>
2026-08-04 15:45:48 -0500

    chore: return unncessary change

--- event_routing_backends/processors/mixins/base_transformer.py
+++ event_routing_backends/processors/mixins/base_transformer.py
@@ -184,7 +184,11 @@ class BaseTransformerMixin:
 
         if result is None:
             if required:
-                raise ValueError('Could not get value for {} in event "{}"'.format(key, self.event.get("name", None)))
+                raise ValueError(
+                    'Could not get value for {} in event "{}"'.format(
+                        key, self.event.get("name", None)
+                    )
+                )
 
         return result
 
